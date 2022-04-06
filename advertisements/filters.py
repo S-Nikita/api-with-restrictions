0b2de2c6dd24e81b246c4712cb49c7ea8623ed9b
S-Nikita <lzlsubzerolzl@gmail.com>
2022-04-06 17:46:01 +0300

    Added status filter

--- advertisements/filters.py
+++ advertisements/filters.py
@@ -15,4 +15,4 @@ class AdvertisementFilter(filters.FilterSet):
 
     class Meta:
         model = Advertisement
-        fields = ['creator', 'created_at']
+        fields = ['creator', 'created_at', 'status']
